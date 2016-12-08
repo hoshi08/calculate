d3acc5c76c38de69d488448a3c8ef05446545fe0
hoshi <h08kou01@gmail.com>
2016-12-08 18:56:04 +0900

    test

---
+++ for文/src/for文/keisan.java
@@ -0,0 +1,10 @@
+public class (keisan.java {
+
+	public static void main(String[] args){
+		  System.out.println("こんにちは");
+	for(int i = 0 ;i < 5 ; i++){
+		 System.out.println("こんにちは")；
+	  }
+
+	}
+}
