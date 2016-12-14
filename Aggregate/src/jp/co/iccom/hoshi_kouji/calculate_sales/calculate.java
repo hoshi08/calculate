070157f29294c6b197443413afed10e0d111da53
hoshi <h08kou01@gmail.com>
2016-12-14 11:51:01 +0900

    支店指定ファイル、商品指定ファイル読み込み完了

--- Aggregate/src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
+++ Aggregate/src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
@@ -1,5 +1,6 @@
 package jp.co.iccom.hoshi_kouji.calculate_sales;
 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,38 +12,67 @@ public class calculate {
 
 	public static void main(String[] args) {
 		System.out.println(args[0]);
-
 		File file = new File(args[0], "blanch.lst");
-		File file1 = new File( file, "commodity.lst");
+		File file1 = new File(args[0], "commodity.lst");
+
+
 
 
 		// existsはフォルダの存在があるかないかを確認するメソッド
-		if (file1.exists()) {
+		if (file.exists()) {
 
-			System.out.println("ファイルは存在し{ます");
+			System.out.println("ファイルは存在します");
 		} else {
 			System.out.println("ファイルは存在しません");
 		}
 
-		HashMap<String, String> blanchcode = new HashMap<String,String>();
+		HashMap<String, String> blanchcode = new HashMap<String, String>();
 
 		// Hashmapを宣言する
-	try {
-			FileReader fr = new FileReader(file1);
-			BufferedReader br = new BufferedReader(fr);
-			while ((str = br.readLine()) != null) {
-				 // strをsplitする
-				String array[] = str.split( "," );
-				// hashmapにputする
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new  BufferedReader(fr);
+			while((str = br.readLine()) != null) {
+				String array[] = str.split(",");
+
+				if (array[0].matches("^\\d{3}")) {
+					System.out.println("チェック結果：OK");
+				} else {
+					System.out.println("支店定義ファイルが不正です");
+				}
+
 				blanchcode.put(array[0], array[1]);
+				System.out.println( blanchcode.entrySet());
 			}
+			br.close();
+		} catch(IOException e) {
+			System.out.println(e);
 
 
-			System.out.println(blanchcode.entrySet());
+		}
+
+			if (file1.exists()) {
+
+				System.out.println("支店定義ファイルは存在します");
+		} else {
+			System.out.println("支店定義ファイルは存在しません");
+		}
+
+
+		HashMap<String, String> commodity = new HashMap<String,String>();
+
+		try {
+			FileReader fr = new FileReader(file1);
+			BufferedReader br = new  BufferedReader(fr);
+			while((str = br.readLine()) != null) {
+				String array[] = str.split(",");
+				commodity.put(array[0], array[1]);
+			}
+			System.out.println( commodity.entrySet());
 
 			br.close();
-		} catch (IOException e) {
-			System.out.println(e);
+		} catch(IOException a) {
+
+		}
 		}
 	}
-}
