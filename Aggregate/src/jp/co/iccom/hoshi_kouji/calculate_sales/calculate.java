6b146f1030eefa976624632d62837dd9a8847eab
hoshi <h08kou01@gmail.com>
2016-12-15 18:02:04 +0900

    処理内容1.2終了

--- Aggregate/src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
+++ Aggregate/src/jp/co/iccom/hoshi_kouji/calculate_sales/calculate.java
@@ -1,26 +1,28 @@
 package jp.co.iccom.hoshi_kouji.calculate_sales;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class calculate {
 	private static String str;
 
-	public static void main(String[] args) {
-		System.out.println(args[0]);
+	public static void main(String args[]) {
+
 		File file = new File(args[0], "blanch.lst");
 		File file1 = new File(args[0], "commodity.lst");
-		File file2 = new File(args[0], "00000001.rcd");
 
+		File dir = new File(args[0]);
+		File files[] = dir.listFiles();
 
+		// 処理1
+		// existsはフォルダの存在があるかないかを確認す+qるメソッド
+		if (file.exists())
 
-
-		// existsはフォルダの存在があるかないかを確認するメソッド
-		if (file.exists()) {
+		{
 
 			System.out.println("");
 		} else {
@@ -32,8 +34,8 @@ public class calculate {
 		// Hashmapを宣言する
 		try {
 			FileReader fr = new FileReader(file);
-			BufferedReader br = new  BufferedReader(fr);
-			while((str = br.readLine()) != null) {
+			BufferedReader br = new BufferedReader(fr);
+			while ((str = br.readLine()) != null) {
 				String array[] = str.split(",");
 
 				if (array[0].matches("^\\d{3}")) {
@@ -43,46 +45,68 @@ public class calculate {
 				}
 
 				blanchcode.put(array[0], array[1]);
-				System.out.println( blanchcode.entrySet());
+				System.out.println(blanchcode.entrySet());
 			}
 			br.close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			System.out.println(e);
 
-
 		}
+		// 処理2
+		if (file1.exists()) {
 
-			if (file1.exists()) {
-
-				System.out.println("");
-			} else {
+			System.out.println("");
+		} else {
 
 			System.out.println("商品定義ファイルは存在しません");
 
-			}
+		}
 
-		HashMap<String, String> commodity = new HashMap<String,String>();
+		HashMap<String, String> commodity = new HashMap<String, String>();
 
 		try {
 			FileReader fr = new FileReader(file1);
-			BufferedReader br = new  BufferedReader(fr);
-			while((str = br.readLine()) != null) {
+			BufferedReader br = new BufferedReader(fr);
+			while ((str = br.readLine()) != null) {
 				String array[] = str.split(",");
 
-				if (array[0].matches("^[A-Z0-9]{8}$")) {
+				if (array[0].matches("^[0-9A-Z]{8}$")) {
 					System.out.println("");
 				} else {
 					System.out.println("商品定義ファイルのファーマットが不正です");
 				}
 
 				commodity.put(array[0], array[1]);
-				System.out.println( commodity.entrySet());
+				System.out.println(commodity.entrySet());
 
 			}
 
 			br.close();
-		} catch(IOException a) {
+		} catch (IOException a) {
 			System.out.println(a);
+
+		}
+		// 処理3
+
+		ArrayList<File> foo = new ArrayList<File>();
+		// ArrayListに1つずつ格納処理
+		for (File f : files) {
+			// rcdファイルかどうかの判定
+			if (f.getName().matches("^\\d{8}.rcd$")) {
+				foo.add(f);
+				System.out.println(f);
+			}
+
+			// 連番チェック
+			// for (File f : foo) {
+			// System.out.println(f);
+
+			// }
+			// // 集計
+			// for (File f : foo) {
+			// System.out.println(f);
+			// try{
+			// }
 
 		}
 	}
